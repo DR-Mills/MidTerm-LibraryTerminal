c5d7957aa1308dcab272cca9216483d467ed0dd2
Dustin Mills <98778591+DR-Mills@users.noreply.github.com>
2022-05-26 16:01:10 -0400

    added Validator and browse menu logic

--- LibraryTerminal/src/LibraryApp.java
+++ LibraryTerminal/src/LibraryApp.java
@@ -5,41 +5,58 @@ import java.util.Scanner;
 public class LibraryApp {
 	static Validator val = new Validator();
 	static Scanner scnr = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
-		
+
 		ArrayList<Movie> movieInventory = new ArrayList<>();
 		ArrayList<Book> bookInventory = new ArrayList<>();
-		movieInventory.add(new Movie("Move1", 100, "Joe Blow", 120));
-		movieInventory.add(new Movie("Move2", 100, "jim Brown", 100));
-		bookInventory.add(new Book("Book1", 100, new ArrayList<String>(Arrays.asList("Mike Jone", "Kyle Johns"))));
-		bookInventory.add(new Book("Book2", 100, new ArrayList<String>(Arrays.asList("Billy Mandy"))));
-		
+		movieInventory.add(new Movie("Move1", 100, "Joe Blow", 120)); // test data
+		movieInventory.add(new Movie("Move2", 100, "jim Brown", 100)); // test data
+		bookInventory.add(new Book("Book1", 100, new ArrayList<String>(Arrays.asList("Mike Jone", "Kyle Johns")))); // test data																											
+		bookInventory.add(new Book("Book2", 100, new ArrayList<String>(Arrays.asList("Billy Mandy")))); // test data
+
 		System.out.println("Welcome to the library");
 		System.out.println(bookInventory);
 		System.out.println(movieInventory);
-		
+
 		boolean userInLibrary = true;
 		int userMainMenuChoice;
 		int userBrowseMenuChoice;
-		
+
 		while (userInLibrary) {
 			System.out.println(
 					"What would you like to do?\n1. Browse\n2. Search\n3. Return\n4. Donate Book\n5. Checkout\n6. Exit");
 			userMainMenuChoice = val.integerWithinRange("Enter number: ", scnr, 1, 6);
-		
+
 			switch (userMainMenuChoice) {
 			case 1:
 				// Browse
 				System.out.println(
 						"1. Browse Books\n2. Browse Movie\n3. Browse Author\n4. Browse Director\n5. Exit To Main Menu");
 				userBrowseMenuChoice = val.integerWithinRange("Enter number: ", scnr, 1, 5);
-			
+
 				switch (userBrowseMenuChoice) {
 				case 1:
 					// Browse Books
-					for(int i = 0; i < bookInventory.size(); i++) {
-						System.out.printf("%-3s%-15s%s%n",i + 1, bookInventory.get(i).getTitle(), bookInventory.get(i).getAuthor());
+					System.out.printf("%-3s%-15s%s%n", "No.", "Book Title", "Author(s)");
+					for (int i = 0; i < bookInventory.size(); i++) {
+						System.out.printf("%-3s%-15s%s%n", i + 1, bookInventory.get(i).getTitle(),
+								bookInventory.get(i).getAuthor());
+					}
+					int userBrowseBookChoice = val.integerInRangeStringToExit(
+							"Enter book number to grab book or type \"main\" to return to main menu: ", "main", scnr, 1,
+							bookInventory.size());
+					if (userBrowseBookChoice == -1) {
+						break;
+					} else {
+						System.out.println(bookInventory.get(userBrowseBookChoice-1).getTitle() + " added to cart.");
+						// need to actually create a cart and add the item to that cart at this point
+						boolean continueBrowse = val.userContinueYorN("Continue Browsing? (y/n): ", scnr);
+						if (continueBrowse) {
+							// recursion ?? do/while loop?
+						} else {
+							break; //should loop back to main menu 
+						}
 					}
 					break;
 				case 2:
@@ -77,8 +94,8 @@ public class LibraryApp {
 
 			}
 
-			//int userCartChoice = menuChoice();
-			//cartMenu(userCartChoice);
+			// int userCartChoice = menuChoice();
+			// cartMenu(userCartChoice);
 		}
 		System.out.println("Thankyou Bye!");
 
